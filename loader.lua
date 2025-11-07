3d71c5e4c241c9043b4518b4db18c125ee8184a9
ago106 <168357108+ago106@users.noreply.github.com>
2025-11-07 23:54:43 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,3 +4,11 @@ Pizza Party id - 5591597781
 Default Lobby id - 
 Game Lobby - 3260590327
 ]]
+
+if game.PlaceId == 5591597781 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ago106/tds-roblox/refs/heads/main/Scripts/Gems.lua"))()
+elseif game.PlaceId == 3260590327 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ago106/tds-roblox/refs/heads/main/Scripts/Money.lua"))()
+else
+    game.Players.LocalPlayer:Kick("Execute in Hardcore/Pizza Party")
+end
